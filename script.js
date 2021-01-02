93add5ca0c5cc761e63d3f9edd4b14a680c15a56
AliferDS <aliferduartesilva@gmail.com>
2021-01-01 23:39:40 -0300

    Memory Game

--- script.js
+++ script.js
@@ -1,31 +1,66 @@
 var images = [];
+var flippedCards = [];
 for(var i = 0; i < 12; i++){
     var img = {
-        src: "assets/" + (i%6) + ".jpg",
+        src: `assets/${i%6}.jpeg`,
         id: i%6
     };
     images.push(img);
 }
-for(var i = 0; i < 12; i++){
-    console.log(images[i].src);
-}
+startGame();
 
-cards();
-
-function cards(){
-    var frontFaces = document.getElementsByClassName("front");
+function startGame(){
+    flippedCards = [];
+    images = randomShuffle(images);
+    
     for(var i = 0; i < 12; i++){
         var card = document.querySelector("#card"+i);
         card.style.left = i%6 === 0 ? 25 + "px" : i%6*185+25 + "px";
         card.style.top = i < 6 ? 25 + "px" : 290 + "px";
 
         card.addEventListener("click", flipCard, false);
-        frontFaces[i].style.background = "url("+images[i].src+");";
-        frontFaces[i].style.repeat = "no-repeat";
+        var frontFace = card.querySelector(".front");
+        frontFace.style.background = `url('${images[i].src}')`
     }
+    document.querySelector("#gameOverModal").style.zIndex = -2;
+    document.querySelector("#gameOverModal").removeEventListener("click", startGame(), false);
 }
 function flipCard(){
-    var faces = this.getElementsByClassName("face");
-    faces[0].classList.toggle("flipped");
-    faces[1].classList.toggle("flipped");
+    if(flippedCards.length < 2 ){
+        var faces = this.getElementsByClassName("face");
+        if(faces[0].classList.length > 2){
+            return;
+        }
+        
+        faces[0].classList.toggle("flipped");
+        faces[1].classList.toggle("flipped");
+
+        flippedCards.push(this);
+    }else{
+        console.log(flippedCards);
+        
+        flippedCards[0].childNodes[1].classList.toggle("flipped");
+        flippedCards[0].childNodes[3].classList.toggle("flipped");
+        flippedCards[1].childNodes[1].classList.toggle("flipped");
+        flippedCards[1].childNodes[3].classList.toggle("flipped");
+        
+
+        flippedCards = [];
+    }
+}
+function gameOver(){
+    var modal = document.querySelector("#gameOverModal");
+    modal.style.zIndex = 10;
+    modal.addEventListener("click", startGame(), false);
+}
+function randomShuffle(array){
+    var newArray = [];
+    while(newArray.length !== array.length){
+        var i = Math.floor(Math.random()*array.length);
+
+        if(newArray.indexOf(array[i]) < 0){
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
 }
